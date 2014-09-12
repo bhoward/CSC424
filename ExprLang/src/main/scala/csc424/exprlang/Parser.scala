763faad8f0f5be37dfb1a620ac68f1a1c51c9142
Brian Howard <bhoward@depauw.edu>
2014-09-12 08:37:58 -0400

    Switched to RegexParsers

--- ExprLang/src/main/scala/csc424/exprlang/Parser.scala
+++ ExprLang/src/main/scala/csc424/exprlang/Parser.scala
@@ -5,40 +5,48 @@ import scala.util.parsing.combinator._
 /**
  * Combinator parser for the Expression Language.
  */
-object Parser extends JavaTokenParsers with PackratParsers {
-  lazy val expr: PackratParser[Expr] =
+object Parser extends RegexParsers with PackratParsers {
+  type P[T] = PackratParser[T]
+  
+  lazy val expr: P[Expr] =
   ( "do" ~ rep1(stmt) ~ "in" ~ expr  ^^ {case _ ~ ss ~ _ ~ e => DoExpr(ss, e)}
   | "let" ~ rep1(decl) ~ "in" ~ expr ^^ {case _ ~ ds ~ _ ~ e => LetExpr(ds, e)}
   | expr ~ addop ~ term              ^^ {case e ~ op ~ t => BinOpExpr(op, e, t)}
   | term
   )
 
-  lazy val term: PackratParser[Expr] =
+  lazy val term: P[Expr] =
   ( term ~ mulop ~ factor ^^ {case t ~ op ~ f => BinOpExpr(op, t, f)}
   | factor
   )
 
-  lazy val factor: PackratParser[Expr] =
+  lazy val factor: P[Expr] =
   ( "-" ~ factor     ^^ {case op ~ f => UnOpExpr(op, f)}
   | "(" ~ expr ~ ")" ^^ {case _ ~ e ~ _ => e}
   | ident            ^^ {case id => IdExpr(id)}
   | wholeNumber      ^^ {case numLit => NumExpr(numLit.toInt)}
   )
   
-  lazy val stmt: PackratParser[Stmt] =
+  lazy val stmt: P[Stmt] =
   ( ident ~ "=" ~ expr ^^ {case id ~ _ ~ e => AssignStmt(id, e)}
   | "read" ~ ident     ^^ {case _ ~ id => ReadStmt(id)}
   | "write" ~ expr     ^^ {case _ ~ e => WriteStmt(e)}
   )
   
-  lazy val decl: PackratParser[Decl] =
+  lazy val decl: P[Decl] =
   ( "val" ~ ident ~ "=" ~ expr ^^ {case _ ~ id ~ _ ~ e => ValDecl(id, e)}
   | "var" ~ ident ~ "=" ~ expr ^^ {case _ ~ id ~ _ ~ e => VarDecl(id, e)}
   )
 
-  lazy val addop: PackratParser[String] = "+" | "-"
+  val addop: Parser[String] = "+" | "-"
 
-  lazy val mulop: PackratParser[String] = "*" | "/" | "%"
+  val mulop: Parser[String] = "*" | "/" | "%"
+  
+  val ident: Parser[String] = """[A-Za-z][A-Za-z0-9]*""".r
+     
+  val wholeNumber: Parser[String] = """[1-9][0-9]*|0""".r
+  
+  override val whiteSpace = """(\s|(//.*\n)|(/\*(?s:.)*?\*/))+""".r
 
   def apply(in: String) = parseAll(expr, in)
 }
