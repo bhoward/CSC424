1aa0462d5bc528122969b31dbdb81bb967efcc9d
Brian Howard <bhoward@depauw.edu>
2014-10-14 17:24:49 -0400

    Merging ExprLang into CtrlLang tree.

---
+++ CtrlLang/src/main/scala/csc424/exprlang/Cell.scala
@@ -0,0 +1,30 @@
+package csc424.exprlang
+
+/**
+ * A Cell acts as a storage place for a value. It is an abstraction
+ * of a memory location.
+ */
+trait Cell {
+  /**
+   * Retrieve the value held in this Cell.
+   */
+  def value: ValueType
+
+  /**
+   * Change the value in this cell, if allowed.
+   */
+  def value_=(n: ValueType): Unit
+}
+
+/**
+ * A ValCell is a read-only storage location. Attempting to modify the
+ * value will throw an error.
+ */
+case class ValCell(value: ValueType) extends Cell {
+  def value_=(n: ValueType) = sys.error("Attempt to change read-only value")
+}
+
+/**
+ * A VarCell is a modifiable storage location. The value may be changed.
+ */
+case class VarCell(var value: ValueType) extends Cell
---
+++ CtrlLang/src/main/scala/csc424/exprlang/package.scala
@@ -0,0 +1,11 @@
+package csc424
+
+/**
+ * The package object holds useful definitions used by the entire package.
+ */
+package object exprlang {
+  /**
+   * ValueType is a synonym for the type of values that may be stored in Cells.
+   */
+  type ValueType = Int
+}
