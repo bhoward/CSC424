07808b1d766bc576379ef499b79b2bd06369d165
Brian Howard <bhoward@depauw.edu>
2014-11-05 13:06:10 -0500

    Updating concurrency examples (Fibonacci)

--- Concurrency/Barber.scala
+++ Concurrency/Barber.scala
@@ -1,24 +1,24 @@
-object Barber extends Actor {
-  val random = new scala.util.Random
-
-  def cutHair(customer: Customer) {
-    customer ! Start
-    println("Barber: Processing customer " + customer)
-    // Randomly take from 1 to 10 seconds
-    Thread.sleep((random.nextInt(10) + 1) * 1000)
-    customer ! Done
-  }
-
-  def act = loop {
-    react {
-      case Enter(customer) => {
-        cutHair(customer)
-        WaitingRoom ! Next
-      }
-
-      case Wait => {
-        println("Barber: No customers. Going to have a sleep")
-      }
-    }
-  }
-}
+//object Barber extends Actor {
+//  val random = new scala.util.Random
+//
+//  def cutHair(customer: Customer) {
+//    customer ! Start
+//    println("Barber: Processing customer " + customer)
+//    // Randomly take from 1 to 10 seconds
+//    Thread.sleep((random.nextInt(10) + 1) * 1000)
+//    customer ! Done
+//  }
+//
+//  def act = loop {
+//    react {
+//      case Enter(customer) => {
+//        cutHair(customer)
+//        WaitingRoom ! Next
+//      }
+//
+//      case Wait => {
+//        println("Barber: No customers. Going to have a sleep")
+//      }
+//    }
+//  }
+//}
--- Concurrency/Fibonacci.scala
+++ Concurrency/Fibonacci.scala
@@ -10,13 +10,37 @@ object Fibonacci {
   def parfib(n: Int): Int =
     if (n < 2) 1
     else {
-      val fibNminus1 = future { fib(n - 1) }
-      val fibNminus2 = future { fib(n - 2) }
-      Await.result(
-        for {
-          f1 <- fibNminus1
-          f2 <- fibNminus2
-        } yield f1 + f2,
-        Duration.Inf)
+      val f1 = future { fib(n - 1) }
+      val f2 = future { fib(n - 2) }
+      val a = Await.result(f1, Duration.Inf)
+      val b = Await.result(f2, Duration.Inf)
+      a + b
     }
+
+  def parfib2(n: Int): Int =
+    if (n < 2) 1
+    else {
+      val f1 = future { fib(n - 1) }
+      val f2 = future { fib(n - 2) }
+      val sum = for {
+        a <- f1
+        b <- f2
+      } yield a + b
+      Await.result(sum, Duration.Inf)
+    }
+
+  def main(args: Array[String]): Unit = {
+    import TimedTest._
+    
+    val (fibResult, fibTime) = time { fib(45) }
+    val (pfResult, pfTime) = time { parfib(45) }
+    val (pf2Result, pf2Time) = time { parfib2(45) }
+    
+    println("Result from plain Fibonacci = " + fibResult)
+    println("  time taken: " + fibTime)
+    println("Result from parallel Fibonacci = " + pfResult)
+    println("  time taken: " + pfTime)
+    println("Result from parallel Fibonacci (v2) = " + pf2Result)
+    println("  time taken: " + pf2Time)
+  }
 }
