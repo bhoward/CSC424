3fff61d451d9fe06eb2fc4a442bbf27be365933f
Brian Howard <bhoward@depauw.edu>
2014-11-06 17:13:54 -0500

    Updating futures section

--- Concurrency/Fibonacci.scala
+++ Concurrency/Fibonacci.scala
@@ -10,8 +10,8 @@ object Fibonacci {
   def parfib(n: Int): Int =
     if (n < 2) 1
     else {
-      val f1 = future { fib(n - 1) }
-      val f2 = future { fib(n - 2) }
+      val f1 = Future { fib(n - 1) }
+      val f2 = Future { fib(n - 2) }
       val a = Await.result(f1, Duration.Inf)
       val b = Await.result(f2, Duration.Inf)
       a + b
@@ -20,8 +20,8 @@ object Fibonacci {
   def parfib2(n: Int): Int =
     if (n < 2) 1
     else {
-      val f1 = future { fib(n - 1) }
-      val f2 = future { fib(n - 2) }
+      val f1 = Future { fib(n - 1) }
+      val f2 = Future { fib(n - 2) }
       val sum = for {
         a <- f1
         b <- f2
@@ -29,18 +29,39 @@ object Fibonacci {
       Await.result(sum, Duration.Inf)
     }
 
+  def parfib3(n: Int): Int =
+    if (n < 2) 1
+    else {
+      val f1 = Future { fib(n - 1) }
+      val f2 = Future { fib(n - 2) }
+      val sum = Promise[Int]
+      f1 onSuccess {
+        case a =>
+          f2 onSuccess {
+            case b =>
+              sum.success(a + b)
+          }
+      }
+      Await.result(sum.future, Duration.Inf)
+    }
+
   def main(args: Array[String]): Unit = {
     import TimedTest._
-    
+
     val (fibResult, fibTime) = time { fib(45) }
-    val (pfResult, pfTime) = time { parfib(45) }
-    val (pf2Result, pf2Time) = time { parfib2(45) }
-    
     println("Result from plain Fibonacci = " + fibResult)
     println("  time taken: " + fibTime)
+
+    val (pfResult, pfTime) = time { parfib(45) }
     println("Result from parallel Fibonacci = " + pfResult)
     println("  time taken: " + pfTime)
+
+    val (pf2Result, pf2Time) = time { parfib2(45) }
     println("Result from parallel Fibonacci (v2) = " + pf2Result)
     println("  time taken: " + pf2Time)
+
+    val (pf3Result, pf3Time) = time { parfib3(45) }
+    println("Result from parallel Fibonacci (v3) = " + pf3Result)
+    println("  time taken: " + pf3Time)
   }
 }
